cc5dff9bf058235ab9d0c62667264f24b34e6ed9
Kevin Deganos and Ming Wen <kevin@>
2016-05-31 16:56:09 -0700

    Edit keg details

--- app/edit-keg-details.component.ts
+++ app/edit-keg-details.component.ts
@@ -6,8 +6,12 @@ import {Keg} from './keg.model';
   inputs: ['keg'],
   template: `
     <div class="keg-form">
-      <h3>Edit Description: </h3>
-      <input [(ngModel)]="keg.name" class="col-sm-8 input-lg keg-form">
+      <h3>Edit Keg: </h3>
+      <label for="kegName">Name</label><input [(ngModel)]="keg.name" class="col-sm-8 input-lg keg-form">
+      <label for="kegBrand">Brand</label><input [(ngModel)]="keg.brand" class="col-sm-8 input-lg keg-form">
+      <label for="kegPrice">Price</label><input [(ngModel)]="keg.price" class="col-sm-8 input-lg keg-form">
+      <label for="kegAlcoholContent">Alcohol Content</label><input [(ngModel)]="keg.alcoholContent" class="col-sm-8 input-lg keg-form">
+      <label for="kegPint">Pints Remaining</label><input [(ngModel)]="keg.pints" class="col-sm-8 input-lg keg-form" id="kegPint">
     </div>
   `
 })
