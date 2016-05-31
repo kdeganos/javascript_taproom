189934623dfb17b0c62c6d1467bf0673dc08a5c9
Kevin Deganos and Ming Wen <ming@>
2016-05-31 16:50:01 -0700

    Edit keg name functionality

---
+++ app/edit-keg-details.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+import {Keg} from './keg.model';
+
+@Component({
+  selector: 'edit-keg-details',
+  inputs: ['keg'],
+  template: `
+    <div class="keg-form">
+      <h3>Edit Description: </h3>
+      <input [(ngModel)]="keg.name" class="col-sm-8 input-lg keg-form">
+    </div>
+  `
+})
+export class EditKegDetailsComponent {
+  public keg: Keg;
+}
